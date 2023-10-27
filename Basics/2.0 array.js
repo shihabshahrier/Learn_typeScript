dea36e5267699ff1e9e955a5db107e8db9e48e9b
arghya-roy7 <96714873+arghya-roy7@users.noreply.github.com>
2023-10-27 20:26:30 +0600

    array

---
+++ Basics/2.0 array.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var names = [];
+names.push("alif");
+console.log(names);
---
+++ Basics/2.0 array.ts
@@ -0,0 +1,6 @@
+const names: string[] = [];
+names.push("alif")
+
+console.log(names)
+
+export{}
