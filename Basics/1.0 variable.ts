7f3e91979997b5864c52217da8b21ce4d623cfea
shihabshahrier <shahrier13@yahoo.com>
2023-10-27 19:54:13 +0600

    variable 02

--- Basics/1.0 variable.ts
+++ Basics/1.0 variable.ts
@@ -6,10 +6,13 @@ let bool_flag: boolean = true;
 
 let another_number: number = 1234;
 
+let onek_boro_number: bigint = 1234567890123456789012345678901234567890n;
+
 // console out
 console.log(amar_name);
 console.log(plus);
 console.log(bool_flag);
 console.log(another_number);
+console.log(onek_boro_number);
 
 export { };
