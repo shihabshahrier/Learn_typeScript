070e74d2e2d544e238d2c8f20a28c44f1e9d1f57
shihabshahrier <shahrier13@yahoo.com>
2023-10-27 19:52:13 +0600

    varible - 01

--- Basics/1.0 variable.ts
+++ Basics/1.0 variable.ts
@@ -1,10 +1,15 @@
-let plus: string="++++++++++++++++++++"
-let name = "ML corporation";
-let flag: boolean= true
-let number: number=1234
+let plus: string = "++++++++++++++++++++";
+
+let amar_name = "ML corporation";
+
+let bool_flag: boolean = true;
+
+let another_number: number = 1234;
+
+// console out
+console.log(amar_name);
 console.log(plus);
-console.log(name);
-console.log(plus);
-console.log(flag);
-console.log(number);
-export {};
+console.log(bool_flag);
+console.log(another_number);
+
+export { };
