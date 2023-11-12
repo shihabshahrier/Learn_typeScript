45d43410cb1c256227c5c2c34448e32c6b4d5ed5
Musaib <musaib.ibn.habib.mikdad@g.bracu.ac.bd>
2023-11-13 00:54:36 +0600

    variables

--- Basics/1.0 variable.js
+++ Basics/1.0 variable.js
@@ -1,11 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var plus = "++++++++++++++++++++";
-var name = "ML corporation";
-var flag = true;
-var number = 1234;
+var amar_name = "ML corporation"; //Implicit type
+var bool_flag = true;
+var another_number = 1234;
+//let onek_boro_number: bigint = 1234567890123456789012345678901234567890n;
+// console out
+console.log(amar_name);
 console.log(plus);
-console.log(name);
-console.log(plus);
-console.log(flag);
-console.log(number);
+console.log(bool_flag);
+console.log(another_number);
--- Basics/1.0 variable.ts
+++ Basics/1.0 variable.ts
@@ -1,18 +1,18 @@
-let plus: string = "++++++++++++++++++++";
+let plus: string = "++++++++++++++++++++"; 
 
-let amar_name = "ML corporation";
+let amar_name = "ML corporation"; //Implicit type
 
 let bool_flag: boolean = true;
 
 let another_number: number = 1234;
 
-let onek_boro_number: bigint = 1234567890123456789012345678901234567890n;
+//let onek_boro_number: bigint = 1234567890123456789012345678901234567890n;
 
 // console out
 console.log(amar_name);
 console.log(plus);
 console.log(bool_flag);
 console.log(another_number);
-console.log(onek_boro_number);
+//console.log(onek_boro_number);
 
 export { };
