0c9c923e21bbe688522b3a160a7cb8b68a3c9b2d
shihabshahrier <shahrier13@yahoo.com>
2023-11-13 20:04:47 +0600

    remove unnes file

--- Basics/2.0 array.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var idiots = [];
-idiots.push("alif");
-console.log(idiots);
-var mc = ["Alif"];
--- Basics/3.0 tuple.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ourTuple;
-ourTuple = [1, true, 'hello world'];
-ourTuple.push(5);
-// readonly tuple
-var ourReadonlyTuple = [5, true, 'The Real Coding God'];
-console.log(ourTuple);
-console.log(ourReadonlyTuple);
